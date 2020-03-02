ecaf84105b478e3c3736fd3193354e9a79f6fca3
larrysmith <larrypsmith415@gmail.com>
2020-03-02 15:03:19 -0800

    Migrate breweries migration to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_222258) do
+ActiveRecord::Schema.define(version: 2020_03_02_225952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "breweries", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "city", null: false
+    t.string "state"
+    t.string "country", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_breweries_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
