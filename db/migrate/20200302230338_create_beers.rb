edec16559adcff7c101fa23e1cb5eb4108f13b71
larrysmith <larrypsmith415@gmail.com>
2020-03-02 15:08:39 -0800

    Write beers migration

--- db/migrate/20200302230338_create_beers.rb
+++ db/migrate/20200302230338_create_beers.rb
@@ -1,8 +1,15 @@
 class CreateBeers < ActiveRecord::Migration[5.2]
   def change
     create_table :beers do |t|
-
+      t.integer :brewery_id, null: false
+      t.string :name, null: false
+      t.string :type, null: false
+      t.string :subtype, null: false
+      t.integer :abv, null: false
+      t.integer :ibu, null: false
+      t.string :description, null: false
       t.timestamps
     end
+    add_index :beers, [:brewery_id, :name], unique: true
   end
 end
