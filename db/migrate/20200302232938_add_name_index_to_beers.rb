1df4fc7bfd9aa4f28f63da62b15176598519db7d
larrysmith <larrypsmith415@gmail.com>
2020-03-02 15:33:12 -0800

    Migrate AddNameIndexToBeers

--- db/migrate/20200302232938_add_name_index_to_beers.rb
+++ db/migrate/20200302232938_add_name_index_to_beers.rb
@@ -1,5 +1,5 @@
 class AddNameIndexToBeers < ActiveRecord::Migration[5.2]
   def change
-    
+    add_index :beers, :name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_232532) do
+ActiveRecord::Schema.define(version: 2020_03_02_232938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_232532) do
     t.datetime "updated_at", null: false
     t.index ["brewery_id", "name"], name: "index_beers_on_brewery_id_and_name", unique: true
     t.index ["brewery_id"], name: "index_beers_on_brewery_id"
+    t.index ["name"], name: "index_beers_on_name"
   end
 
   create_table "breweries", force: :cascade do |t|
