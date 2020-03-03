55d2eb04dd3d127f88f5f963cdf1a3726ddf10bb
larrysmith <larrypsmith415@gmail.com>
2020-03-02 16:40:42 -0800

    Setup React hook on root page

--- frontend/bitter.jsx
+++ frontend/bitter.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Bitter</h1>, root)
+  ReactDOM.render(<p>Welcome to Bitter</p>, root)
 })
