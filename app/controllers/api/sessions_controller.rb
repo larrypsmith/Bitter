ba0233b0eb8a7be79692668d4f6662aba2368f34
larrysmith <larrypsmith415@gmail.com>
2020-03-02 17:38:46 -0800

    Add SessionsController#create and Sessionscontroller#destroy

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password],
+    )
+
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ["Invalid username or password"], status: 401
+    end
+  end
+
+  def destroy 
+    @user = current_user
+    if @user
+      logout
+      render "static_pages/root"
+    else
+      render json: ["No current user"], status: 404
+    end
+  end
 end
