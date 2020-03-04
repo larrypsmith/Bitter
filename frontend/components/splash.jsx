d7178db4479f9ee17dc6f1437bb02dd9073721b5
larrysmith <larrypsmith415@gmail.com>
2020-03-04 15:39:36 -0800

    Style splash page nav links

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default ({ loginDemoUser }) => (
-  <div>
-    <Link to="/login">Log In</Link>
-    <Link to="/signup">Sign Up</Link>
-    <button onClick={loginDemoUser}>Log In as Demo User</button>
-    <h1>BITTER</h1>
-    <h2>Drink Socially</h2>
+  <div className="splash">
+    <img className="bg-img" src="/assets/beer-glasses-splash-page.jpg" alt="beers"/>
+    <div className="bg-img-opacity"></div>
+
+    <header className="splash-header">
+      <button onClick={loginDemoUser} className="splash-header-btn demo-btn">Log In as Demo User</button>
+      <Link to="/login" className="splash-header-btn session-link">Log In</Link>
+      <Link to="/signup" className="splash-header-btn session-link">Create an Account</Link>
+    </header>
+
+    <main>
+      <h1>BITTER</h1>
+      <h2>Drink Socially</h2>
+    </main>
+    
   </div>
 )
