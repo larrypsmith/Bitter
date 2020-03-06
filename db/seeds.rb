3594407d3e43ee8d4e5150dfffda313372fcdcbd
larrysmith <larrypsmith415@gmail.com>
2020-03-06 09:32:46 -0800

    Add seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,17 @@
 
 
 User.delete_all
+demo_user          = User.create(username: "demo",               password: "password")
+beer_lover12       = User.create(username: "beer_lover12",       password: "beerislife")
+the_beer_snob      = User.create(username: "the_beer_snob",      password: "beersnoblol")
+cider_rules        = User.create(username: "cider_rules",        password: "ciderisking")
+bay_area_beer_luvr = User.create(username: "bay_area_beer_luvr", password: "baybeer125")
+i_love_beer        = User.create(username: "i_love_beer",        password: "beeeeeer")
+alex_stevens       = User.create(username: "alex_stevens",       password: "alexlovesbeer")
+jeff_lubinski      = User.create(username: "jeff_lubinski",      password: "jefflovesbeer")
+casey_smith        = User.create(username: "casey_smith",        password: "caseylovesbeer")
+lauren_andrews     = User.create(username: "lauren_andrews",     password: "laurenlovesbeer")
+taylor_miller      = User.create(username: "taylor_miller",      password: "taylor_loves_beer")
 
-demo_user = User.create(username: "demo", password: "password")
+Brewery.delete_all
+lagunitas = Brewery.create()
