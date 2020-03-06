bfbafe9f4142015705f513eacd0af6b262493722
larrysmith <larrypsmith415@gmail.com>
2020-03-06 13:49:30 -0800

    Fix bug where site would not redirect to /breweries upon login

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch } from 'react-router-dom'
+import { Switch, Redirect } from 'react-router-dom'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import LoginFormContainer from './login_form_container'
 import SignupFormContainer from './signup_form_container'
@@ -8,12 +8,19 @@ import NavBar from './nav_bar';
 
 const App = () => (
   <div>
+    <Switch>
+      <AuthRoute path="/login" component={LoginFormContainer} />
+      <AuthRoute path="/signup" component={SignupFormContainer} />
+      <AuthRoute exact path="/" component={SplashContainer} />
+      <AuthRoute path="/" component={() => <Redirect to='/' />} />
+    </Switch>
+
     <ProtectedRoute path="/" component={NavBar} />
 
     <Switch>
-      <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      <AuthRoute exact path="/" component={SplashContainer} />
+      <ProtectedRoute path="/breweries" component={() => <div>BREWERIES COMPONENT</div>} />
+      <ProtectedRoute path="/home" component={() => <div>HOME COMPONENT</div>} />
+      <ProtectedRoute path="/" component={() => <Redirect to="/breweries" />} />
     </Switch>
   </div>
 )
