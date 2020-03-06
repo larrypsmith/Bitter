92454c0eab69eec13c3b1330751b43f45f963de7
larrysmith <larrypsmith415@gmail.com>
2020-03-06 14:47:44 -0800

    Add brewery api util

---
+++ frontend/util/brewery_api_util.js
@@ -0,0 +1,11 @@
+export const fetchBreweries = () => (
+  $.ajax({
+    url: 'api/breweries'
+  })
+)
+
+export const fetchBrewery = breweryId => (
+  $.ajax({
+    url: `api/breweries/${breweryId}`
+  })
+)
