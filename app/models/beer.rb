a69c0498c1217cc3dc5f3ed79d85fec7c8cd4d73
larrysmith <larrypsmith415@gmail.com>
2020-03-09 12:09:35 -0700

    Add validations to Beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,8 @@
 class Beer < ApplicationRecord
+  validates :brewery_id, :name, :beer_type, :abv, :ibu, :description, presence: true
+  validates :brewery_id, uniqueness: { scope: :name }
+  
   belongs_to :brewery
+
+  has_one_attached :profile_picture
 end
