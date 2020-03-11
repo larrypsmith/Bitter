3464a89f1c900aed3f900e1c99553fd227ba5f8c
larrysmith <larrypsmith415@gmail.com>
2020-03-10 23:22:27 -0700

    Add 'RECEIVE_USER' action

--- frontend/reducers/beers_reducer.js
+++ frontend/reducers/beers_reducer.js
@@ -1,9 +1,12 @@
 import { RECEIVE_BREWERY } from '../actions/brewery_actions'
+import { RECEIVE_USER } from '../actions/user_actions';
 
 const beersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_BREWERY:
+      return action.payload.beers;
+    case RECEIVE_USER:
       return action.payload.beers;
     default:
       return state;
