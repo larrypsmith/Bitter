bcc8308ef7b64355f20ecd1bb56857c96f0e7692
larrysmith <larrypsmith415@gmail.com>
2020-03-11 10:55:01 -0700

    Destructure state and ownProps in parameters of mSTP

--- frontend/components/user_show_container.js
+++ frontend/components/user_show_container.js
@@ -3,13 +3,12 @@ import { withRouter } from 'react-router-dom'
 import { fetchUser } from '../actions/user_actions';
 import UserShow from './user_show';
 
-const mSTP = (state, ownProps) => {
-  const user = state.entities.users[ownProps.match.params.id];
+const mSTP = ({ entities: { users, beers, breweries, checkins } }, { match: { params: { id } } }) => {
   return ({
-    user: user,
-    checkins: Object.values(state.entities.checkins),
-    beers: state.entities.beers,
-    breweries: state.entities.breweries
+    user: users[id],
+    checkins: Object.values(checkins),
+    beers: beers,
+    breweries: breweries
   })
 }
 
