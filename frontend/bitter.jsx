694948677befed458904b3a83a78cee5a89f7ad0
larrysmith <larrypsmith415@gmail.com>
2020-04-08 13:48:52 -0700

    Remove test functions from window

--- frontend/bitter.jsx
+++ frontend/bitter.jsx
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import * as SessionAPIUtil from './util/session_api_util';
-import { login, logout } from './actions/session_actions'
-
-import * as BreweryAPIUtil from './util/brewery_api_util';
-import { fetchBreweries, fetchBrewery } from './actions/brewery_actions'
-
-import * as UserAPIUtil from './util/user_api_util';
-import { fetchUser } from './actions/user_actions';
-
-import * as CheckinAPIUtil from './util/checkin_api_util';
-import { fetchCheckins } from './actions/checkin_actions';
-
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -31,22 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
 
-  //TESTING START
-  window.SessionAPIUtil = SessionAPIUtil;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.login = login;
-  window.logout = logout;
-  window.BreweryAPIUtil = BreweryAPIUtil;
-  window.fetchBreweries = fetchBreweries;
-  window.fetchBrewery = fetchBrewery;
-  window.UserAPIUtil = UserAPIUtil;
-  window.fetchUser = fetchUser
-  window.CheckinAPIUtil = CheckinAPIUtil;
-  window.fetchCheckins = fetchCheckins;
-  //TESTING END
-
-  
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root)
 })
