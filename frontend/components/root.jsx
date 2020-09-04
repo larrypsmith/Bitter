50924ce0d8e2adb97d69c014362fa6e3143ed2c8
Larry Smith <larrypsmith415@gmail.com>
2020-09-03 18:08:10 -0700

    Setup material-ui and theme

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 import App from "./App";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fc0'
+    },
+    link: '#c60'
+  }
+});
+
+
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </HashRouter>
   </Provider>
 );
